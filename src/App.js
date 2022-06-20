335e52023020f0ac76db63422ac2521ded6ccc6a
odadki <93558662+odadki@users.noreply.github.com>
2022-06-20 16:13:56 -0700

    updating directory page with directory component and category item

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import CategoryItem from "./components/category-item/category-item.component";
 import "./categories.styles.scss";
 
 const App = () => {
@@ -5,35 +6,34 @@ const App = () => {
     {
       id: 1,
       title: "Hats",
+      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
     },
     {
       id: 2,
       title: "Jackets",
+      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
     },
     {
       id: 3,
       title: "Sneakers",
+      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
     },
     {
       id: 4,
       title: "Womens",
+      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
     },
     {
       id: 5,
       title: "Mens",
+      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
     },
   ];
 
   return (
     <div className="categories-container">
-      {categories.map(({ title }) => (
-        <div className="category-container">
-          <div className="background-image" />
-          <div className="category-body-container">
-            <h2>{title}</h2>
-            <p>Shop now</p>
-          </div>
-        </div>
+      {categories.map((category) => (
+        <CategoryItem key={category.id} category={category} />
       ))}
       ;
     </div>
---
+++ src/directory/directory.component.js
@@ -0,0 +1,16 @@
+import React from "react";
+import CategoryItem from "../components/category-item/category-item.component";
+import "./categories.styles.scss";
+
+const Directory = ({ categories }) => {
+  return (
+    <div className="categories-container">
+      {categories.map((category) => (
+        <CategoryItem key={category.id} category={category} />
+      ))}
+      ;
+    </div>
+  );
+};
+
+export default Directory;
